b2e154a975f78ecd0929d84cbe2ff8abcb6a1265
Petar Belchev <petar9004@gmail.com>
2022-08-13 11:12:29 +0300

    Create 01. Print an Array with a Given Delimiter.js

---
+++ JS Advanced/02. Arrays and Nested Arrays - Exercise/01. Print an Array with a Given Delimiter.js
@@ -0,0 +1,21 @@
+function solve(arr, str) {
+    console.log(arr.join(str));
+}
+
+solve([
+    'One',
+    'Two',
+    'Three',
+    'Four',
+    'Five'],
+    '-'
+);
+solve([
+    'How about no?',
+    'I',
+    'will',
+    'not',
+    'do',
+    'it!'],
+    '_'
+);

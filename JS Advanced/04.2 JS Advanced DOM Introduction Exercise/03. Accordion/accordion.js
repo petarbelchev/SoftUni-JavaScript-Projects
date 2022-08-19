d505232f64c0148892ebe4e8a09e65588b04442f
Petar Belchev <petar9004@gmail.com>
2022-08-19 15:00:07 +0300

    Added a Accordion task

---
+++ JS Advanced/04.2 JS Advanced DOM Introduction Exercise/03. Accordion/accordion.js
@@ -0,0 +1,10 @@
+function toggle() {
+    let button = document.querySelector('.button');
+    if (button.textContent == 'More') {
+        button.textContent = 'Less';   
+        document.getElementById('extra').style.display = 'block';     
+    } else if (button.textContent == 'Less') {
+        button.textContent = 'More';
+        document.getElementById('extra').style.display = 'none';
+    }
+}

5c5a3bde6558a052a72fcc64bde59967e4bc548b
Petar Belchev <petar9004@gmail.com>
2022-10-02 19:40:29 +0300

    Added a Bus Stop task

---
+++ JS Application/02.2 Asynchronous Programming - Exercise/01.Bus-Stop/app.js
@@ -0,0 +1,26 @@
+function getInfo() {
+    let inputField = document.getElementById('stopId');
+    let stopId = inputField.value;
+    inputField.value = '';
+
+    let stopNameField = document.getElementById('stopName');
+    let busesList = document.getElementById('buses');
+    busesList.innerHTML = '';
+
+    fetch(`http://localhost:3030/jsonstore/bus/businfo/${stopId}`)
+        .then(response => {
+            if (stopId === '' || response.status != 200){
+                throw new Error();
+            }
+            return response.json();
+        })
+        .then(data => {
+            stopNameField.textContent = data.name;
+            for (let [busId, time] of Object.entries(data.buses)) {
+                let li = document.createElement('li');
+                li.textContent = `Bus ${busId} arrives in ${time} minutes`;
+                busesList.appendChild(li);
+            }
+        })
+        .catch(() => stopNameField.textContent = "Error");
+}

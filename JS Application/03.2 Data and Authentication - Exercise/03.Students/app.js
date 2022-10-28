9b456a4a2f34efe28ff695b9f7e7214ed0e8069b
Petar Belchev <petar9004@gmail.com>
2022-10-28 15:18:08 +0300

    Update app.js

--- JS Application/03.2 Data and Authentication - Exercise/03.Students/app.js
+++ JS Application/03.2 Data and Authentication - Exercise/03.Students/app.js
@@ -1,33 +1,51 @@
 window.addEventListener('load', () => {
     let url = 'http://localhost:3030/jsonstore/collections/students'
+    let tableBody = document.querySelector('tbody')
 
-    fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            let tableBody = document.querySelector('tbody')
+    loadStudents()
 
-            Object.values(data).forEach(student => {
-                tableBody.appendChild(createTableRow(student))
-            })
-
-            document.querySelector('form').addEventListener('submit', (e) => {
-                e.preventDefault()
-                let formData = new FormData(e.currentTarget)
-                let firstName = formData.get('firstName')
-                let lastName = formData.get('lastName')
-                let facultyNumber = formData.get('facultyNumber')
-                let grade = formData.get('grade')
-
-                if (firstName.length !== 0 && typeof (firstName) == 'string' &&
-                    lastName.length !== 0 && typeof (lastName) == 'string' &&
-                    facultyNumber.length !== 0 && Number(facultyNumber) &&
-                    grade.length !== 0 && Number(grade)) {
-
-                    let data = { firstName, lastName, facultyNumber, grade }
-                    tableBody.appendChild(createTableRow(data))
+    function loadStudents() {
+        fetch(url)
+            .then(res => {
+                if (res.status == 200) {
+                    return res.json()
                 }
             })
-        })
+            .then(data => {
+                tableBody.innerHTML = ''
+                Object.values(data).forEach(student => {
+                    tableBody.appendChild(createTableRow(student))
+                })
+            })
+    }
+
+    document.querySelector('form').addEventListener('submit', (e) => {
+        e.preventDefault()
+        let formData = new FormData(e.currentTarget)
+        let firstName = formData.get('firstName')
+        let lastName = formData.get('lastName')
+        let facultyNumber = formData.get('facultyNumber')
+        let grade = formData.get('grade')
+
+        if (firstName.length !== 0 && typeof (firstName) == 'string' &&
+            lastName.length !== 0 && typeof (lastName) == 'string' &&
+            facultyNumber.length !== 0 && Number(facultyNumber) &&
+            grade.length !== 0 && Number(grade)) {
+
+            let data = { firstName, lastName, facultyNumber, grade }
+
+            fetch(url, {
+                method: 'post',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify(data)
+            })
+                .then(res => {
+                    if (res.status == 200) {
+                        loadStudents()
+                    }
+                })
+        }
+    })
 
     function createTableRow(data) {
         let tr = document.createElement('tr')
